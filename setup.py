5158612379cd50420e426642f68f465e5c83feab
danthe3rd <43445237+danthe3rd@users.noreply.github.com>
2025-10-03 06:50:08 -0700

    Fix build by adding -dlink always

--- setup.py
+++ setup.py
@@ -37,8 +37,13 @@ if __name__ == '__main__':
 
     cxx_flags = ['-O3', '-Wno-deprecated-declarations', '-Wno-unused-variable', '-Wno-sign-compare', '-Wno-reorder', '-Wno-attributes']
     nvcc_flags = ['-O3', '-Xcompiler', '-O3']
-    sources = ['csrc/deep_ep.cpp', 'csrc/kernels/runtime.cu', 'csrc/kernels/layout.cu', 'csrc/kernels/intranode.cu']
     include_dirs = ['csrc/']
+    sources = [
+        'csrc/deep_ep.cpp',
+        'csrc/kernels/runtime.cu',
+        'csrc/kernels/layout.cu',
+        'csrc/kernels/intranode.cu'
+    ]
     library_dirs = []
     nvcc_dlink = []
     extra_link_args = []
@@ -47,6 +52,7 @@ if __name__ == '__main__':
     if disable_nvshmem:
         cxx_flags.append('-DDISABLE_NVSHMEM')
         nvcc_flags.append('-DDISABLE_NVSHMEM')
+        nvcc_dlink.extend(['-dlink'])
     else:
         sources.extend(['csrc/kernels/internode.cu', 'csrc/kernels/internode_ll.cu'])
         include_dirs.extend([f'{nvshmem_dir}/include'])
